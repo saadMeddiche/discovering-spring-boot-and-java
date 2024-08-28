17ac29db25236bc608e6aa2839bde06981fea25b
smeddiche <s.meddiche@itroadgroup.com>
2024-08-28 14:52:04 +0100

    intialize spring boot project

---
+++ src/main/java/com/discovering/springbootandjava/DiscoveringSpringBootAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.discovering.springbootandjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveringSpringBootAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveringSpringBootAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discovering/springbootandjava/DiscoveringSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovering.springbootandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveringSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

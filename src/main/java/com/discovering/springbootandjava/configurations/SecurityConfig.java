a1701a2e0634444cfbf343a82c8f1dee6e50714e
smeddiche <s.meddiche@itroadgroup.com>
2024-08-29 12:13:55 +0100

    create basic security configuration

--- src/main/java/com/discovering/springbootandjava/configurations/SecurityConfig.java
+++ src/main/java/com/discovering/springbootandjava/configurations/SecurityConfig.java
@@ -1,3 +1,37 @@
 package com.discovering.springbootandjava.configurations;
 
-public class SecurityConfig { }
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http
+               .authorizeHttpRequests(request ->
+                       request.anyRequest().permitAll()
+               )
+               .formLogin(Customizer.withDefaults())
+               .logout(Customizer.withDefaults())
+               .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("123456789")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
